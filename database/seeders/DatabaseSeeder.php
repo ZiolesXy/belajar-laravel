a4b767d2a68f545a54ef77ad7552123df54f6da8
ZiolesXy <eaglegaming3605@gmail.com>
2025-09-02 15:31:51 +0700

    part 13

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Menu;
+use App\Models\Posts;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Posts::create([
+        //     'title' => 'Judul Post 1',
+        //     'slug' => 'judul-post-1',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+        //     'category_id' => 1,
+        //     'author_id' => 1,
+        // ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call([CategorySeeder::class, UserSeeder::class]);
+        Posts::factory(100)->recycle([
+            Category::all(),
+            User::all(),
+        ])->create();
     }
 }
